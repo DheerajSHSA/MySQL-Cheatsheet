d9f6242da87539341c7d95f85880a64ed3767a6a
Dheeraj kashyap <Dheerajkashyap4889@gmail.com>
2022-12-11 18:56:03 +0530

    Basics of Select Statement

---
+++ SelectStatement.sql
@@ -0,0 +1,17 @@
+-- Selecting a Database
+USE sql_store;
+
+-- Fetch All(*) the data from customers(Table name)
+SELECT *
+FROM customers;
+
+-- Fetch All the data from customers where id = 1 
+SELECT *
+FROM customers
+WHERE customer_id = 1;
+
+-- Fetch All the data and Order(sort) by the customer name
+SELECT *
+FROM customers
+WHERE customer_id = 1
+ORDER BY first_name;
