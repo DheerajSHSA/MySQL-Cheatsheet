01ab47b7750974bdc657ce929d007df78e911638
Dheeraj kashyap <Dheerajkashyap4889@gmail.com>
2022-12-12 13:10:37 +0530

    Fetching data from different databases using join

--- SelectStatement.sql
+++ SelectStatement.sql
@@ -155,3 +155,10 @@ oi.quantity,
 p.unit_price
 FROM order_items oi
 JOIN products p ON oi.product_id = p.product_id;
+
+-- Fetch data from two different databases
+SELECT *
+FROM order_items oi
+JOIN sql_inventory.products p
+	ON oi.product_id = p.product_id;
+
