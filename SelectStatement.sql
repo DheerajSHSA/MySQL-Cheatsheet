3dc1052e7e32541a1d7a565f991d811b18123807
Dheeraj kashyap <Dheerajkashyap4889@gmail.com>
2022-12-12 13:27:13 +0530

    IMPLICIT JOIN

--- SelectStatement.sql
+++ SelectStatement.sql
@@ -162,3 +162,30 @@ FROM order_items oi
 JOIN sql_inventory.products p
 	ON oi.product_id = p.product_id;
 
+SELECT *
+FROM order_items oi
+JOIN sql_inventory.products p
+	ON oi.product_id = p.product_id
+
+-- Fetch status id
+SELECT
+	o.order_id,
+    o.order_date,
+    c.first_name,
+    c.last_name,
+    os.name as "Status"
+FROM orders o
+JOIN customers c
+	ON o.customer_id = c.customer_id
+JOIN order_statuses os
+	ON o.status = os.order_status_id
+
+-- Implicit Join
+-- SELECT *
+-- FROM orders o
+-- JOIN customers c
+-- 	ON o.customer_id = c.customer_id
+
+SELECT *
+FROM orders o, customers c
+WHERE o.customer_id = c.customer_id
